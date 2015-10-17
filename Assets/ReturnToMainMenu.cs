aa44361a39142cbdc1626eb387735a8bf8f93000
Richard Henderson <Rich121212@hotmail.com>
2015-10-17 06:12:32 -0400

    Amazing Scenes/Menus/Win.unity complete with sound

---
+++ Assets/ReturnToMainMenu.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToMainMenu : MonoBehaviour {
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape)) Application.LoadLevel("MainMenu");
+	}
+}
